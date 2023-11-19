4b005acd7e7751acc739e48128b899cacfa2e41c
davidkorenblyum <dakorenblyum@gmail.com>
2023-11-19 17:06:35 -0500

    fixed even more bugs

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
+        console.log(entry)
         if(entry.isIntersecting) {
             entry.target.classList.add('show');
         }
     });
-});
+}, { threshold: .025 });
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
